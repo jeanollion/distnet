fixed dy computation

dlutils/h5_dy_iterator.py
@@ -28,10 +28,15 @@ class H5dyIterator(H5TrackingIterator):
 		# label and prev label
 		labelIms = self._get_batches_of_transformed_samples_by_channel(index_ds, index_array, 1, False, aug_param_array, perform_augmentation=True)
 		prevlabelIms = self._get_batches_of_transformed_samples_by_channel(index_ds, index_array, 2, False, aug_param_array, perform_augmentation=True)
-
+		no_neigh_key = "no_prev"
+		no_neigh_aug_key = "no_prev_aug"
 		dyIm = np.zeros(labelIms.shape[:-1]+(1,), dtype=self.dtype)
 		for i in range(labelIms.shape[0]):
-			dyIm[i][...,0] = compute_dy(labelIms[i,...,1], labelIms[i,...,0], prevlabelIms[i,...,0])
+			if aug_param_array is not None and (aug_param_array[i][0].get(no_neigh_key, False) or aug_param_array[i][0].get(no_neigh_aug_key, False)):
+				prevLabelIm = None
+			else:
+				prevLabelIm = prevlabelIms[i,...,0]
+			dyIm[i][...,0] = compute_dy(labelIms[i,...,1], labelIms[i,...,0], prevLabelIm)
 
 		if len(self.output_channels)>1:
 			all_channels = [self._get_batches_of_transformed_samples_by_channel(index_ds, index_array, chan_idx, False, aug_param_array, perform_augmentation=True) for chan_idx in self.output_channels[1:]]
@@ -42,9 +47,9 @@ class H5dyIterator(H5TrackingIterator):
 
 # dy computation utils
 def get_prev_lab(labelIm_of_prevCells, labelIm, label, center):
-	prev_lab = labelIm_of_prevCells[int(round(center[0])), int(round(center[1]))]
+	prev_lab = int(labelIm_of_prevCells[int(round(center[0])), int(round(center[1]))])
 	if prev_lab==0: # check that mean value is also 0
-		prev_lab = round(mean(labelIm_of_prevCells, labelIm, label))
+		prev_lab = int(round(mean(labelIm_of_prevCells, labelIm, label)))
 	return prev_lab
 
 def get_labels_and_centers(labelIm):
@@ -59,16 +64,20 @@ def compute_dy(labelIm, labelIm_prev, labelIm_of_prevCells):
 	labels, centers = get_labels_and_centers(labelIm)
 	if len(labels)==0:
 		return np.zeros(labelIm.shape, dtype=labelIm.dtype)
+	labels_prev, centers_prev = get_labels_and_centers(labelIm_prev)
+	if labelIm_of_prevCells is None: # previous image is (augmented) current image
+		labels_of_prev = labels
+	else:
+		labels_of_prev = [get_prev_lab(labelIm_of_prevCells, labelIm, label, center) for label, center in zip(labels, centers)]
 
 	dyIm = np.copy(labelIm)
-	prevLabs = [get_prev_lab(labelIm_of_prevCells, labelIm, label, centers[i]) for i, label in enumerate(labels)]
-	labels_prev, centers_prev = get_labels_and_centers(labelIm_prev)
-
-	for i, label in enumerate(labels):
-		if label not in labels_prev:
-			dyIm[dyIm == label] = 0
+	for label, center, label_prev in zip(labels, centers, labels_of_prev):
+		if label_prev not in labels_prev:
+			dyIm[dyIm == label] = dyIm.shape[0] # not found -> out of the image ? or zero ?
 		else:
-			i_prev = labels_prev.index(label)
-			dy = centers[i][0] - centers_prev[i_prev][0] # 0 is y
+			i_prev = labels_prev.index(label_prev)
+			dy = center[0] - centers_prev[i_prev][0] # axis 0 is y
+			if dy==0:
+				dy = 0.1 # not 0
 			dyIm[dyIm == label] = dy
 	return dyIm