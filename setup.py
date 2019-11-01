709dba15995b2e30b0d5a21fde9ad83f63d76bf1
jollion <jean.ollion@polytechnique.org>
2019-11-01 17:38:27 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3',
-    install_requires=['h5py>=2.9,==1.14.0', 'numpy', 'scipy', 'scikit-learn', 'keras']
+    install_requires=['h5py>=2.9', 'numpy', 'scipy', 'scikit-learn', 'keras']
 )
