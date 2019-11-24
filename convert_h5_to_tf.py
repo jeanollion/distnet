b9e57f05d61c1c87e91ff9852efdc622e471844a
jollion <jean.ollion@polytechnique.org>
2019-11-24 11:41:13 +0100

    use export bundle

--- convert_h5_to_tf.py
+++ convert_h5_to_tf.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('/data/Dev/DL_Utils')
 import dlutils.keras_models as km
-from dlutils.utils import export_model_graph
+from dlutils.utils import export_model_graph, export_model_bundle
 #model_edm, model_dy, model_dy_predict = get_edm_displacement_untangled_model(filters_dy=24, filters_edm=16)
 #model_dy.load_weights("/data/Images/MOP/data_segDis_resampled/seg_edm16dy24_model2_cp2.h5")
 #model_edm.load_weights("/data/Images/MOP/data_segDis_resampled/seg_edm16_model_lum_noise_cp2.h5")
@@ -12,4 +12,6 @@ from dlutils.utils import export_model_graph
 
 model = km.get_unet_model(image_shape=(256,32), filters=48, n_down=4, n_hourglass=4, n_input_channels=2, n_outputs=3, n_output_channels=[1, 4, 2], out_activations=["linear", "softmax", "linear"])
 model.load_weights("/data/Images/Phase/DL_models/seg_track_48x4_cpL.h5")
-export_model_graph(model, "/data/Images/Phase/DL_models/", "model_dy_cat_edm_48x4.pb", input_names=["input"])
+#export_model_graph(model, "/data/Images/Phase/DL_models/", "model_dy_cat_edm_48x4.pb", input_names=["input"])
+
+export_model_bundle(model, "/data/Images/Phase/DL_models/model_dy_cat_edm_48x4")
