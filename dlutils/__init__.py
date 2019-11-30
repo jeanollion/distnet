5c250f419f65162a8e3f5eb87edf071af64146a4
jollion <jean.ollion@polytechnique.org>
2019-11-30 12:28:34 +0100

    removed CLR

--- dlutils/__init__.py
+++ dlutils/__init__.py
@@ -1,6 +1,5 @@
 name = "dlutils"
-#from .utils import *
-#from .pre_processing_utils import *
+
 from .atomic_file_handler import AtomicFileHandler
 from .index_array_iterator import IndexArrayIterator
 from .image_data_generator_mm import ImageDataGeneratorMM
@@ -8,4 +7,3 @@ from .h5_multichannel_iterator import H5MultiChannelIterator
 from .h5_tracking_iterator import H5TrackingIterator
 from .h5_dy_iterator import H5dyIterator
 from .patched_model_checkpoint import PatchedModelCheckpoint
-from .CLR import CyclicLR
