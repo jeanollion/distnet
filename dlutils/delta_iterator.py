842a99d244b38e87bfc233fda1b26cbb59a4ed92
jollion <jean.ollion@polytechnique.org>
2019-12-12 15:45:28 +0100

    added iterator for delta software

---
+++ dlutils/delta_iterator.py
@@ -0,0 +1,62 @@
+from dlutils import TrackingIterator
+from random import choice
+from scipy.ndimage.measurements import center_of_mass
+import numpy as np
+
+class DeltaIterator(TrackingIterator):
+    def __init__(self,
+        h5py_file_path,
+        channel_keywords=['/raw', '/regionLabels', '/prevRegionLabels'], # channel @1 must be label & @2 previous label
+        group_keyword=None,
+        image_data_generators=None,
+        batch_size=32,
+        shuffle=True,
+        perform_data_augmentation=True,
+        seed=None,
+        dtype='float32'):
+        assert len(channel_keywords)==3, "Channels must be: raw, regions labels and preious region labels"
+        super().__init__(h5py_file_path, channel_keywords, [0, 1], [2], [True, True, False], [False, False, False], [1, 2], 1, None, group_keyword, image_data_generators, batch_size, shuffle, perform_data_augmentation, seed)
+
+    def _get_input_batch(self, batch_by_channel, ref_chan_idx, aug_param_array):
+        # current frame: remove all cells but no_next
+        # previous cells
+        rawIms = batch_by_channel[0] # nothing to do
+        labels = batch_by_channel[1] # channel 0 -> choise one label and mark it in data_augmentation, channel 1 : binarize
+        return_labels = np.copy(labels)
+        return_labels[...,1][return_labels[...,1]>0]=1 # next : binarize
+        for i in range(labels.shape[0]):
+            all_labels = np.unique(labels[i,...,0])
+            if len(all_labels)==0:
+                return_labels[i,...,1] = 0
+                aug_param_array[i][1]["label"] = 0
+            else:
+                label = choice(all_labels)
+                return_labels[i,...,0][return_labels[i,...,0] != label] = 0 # erase all other labels
+                aug_param_array[i][1]["label"] = label
+        return [rawIms, return_labels]
+
+    def _get_output_batch(self, batch_by_channel, ref_chan_idx, aug_param_array):
+        # dy is computed and returned instead of labels & prevLabels
+        labelIms = batch_by_channel[1][...,1]
+        prevLabelIms = batch_by_channel[2][...,0]
+        # return 1 channel: 0 = background, 1 = next 2 = next (2nd daughter)
+        target = np.zeros(shape=labelIms.shape+(1,))
+        for i in range(target.shape[0]):
+            labelIm = labelIms[i]
+            prevLabelIm = prevLabelIms[i]
+            label = aug_param_array[i][1]["label"]
+            if label>0:
+                labels = np.unique(labelIm[prevLabelIm == label])
+                if len(labels)==1:
+                    target[i,...,0][labelIm == labels[0]] = 1
+                elif len(labels)>1: # division: set the uppermost cell as 1 and the others as 2
+                    # find the upper most cell
+                    centers = center_of_mass(labelIm, labelIm, index=labels)
+                    centers = [c[0] for c in centers]
+                    idx_min = centers.index(min(centers))
+                    label1 = labels[idx_min]
+                    label2 = [l for l in labels if l!=label1]
+                    # label the cells
+                    target[i,...,0][labelIm== label1] = 1
+                    target[i,...,0][labelIm== label2] = 2
+        return target
