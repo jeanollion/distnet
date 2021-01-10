ca8cd6173b259ba9c832aa5adc09817fa3ae5520
jeanollion <smogollion@yahoo.fr>
2021-01-10 19:00:43 +0100

    init ConstantConvolution2D

--- distnet/keras_models/__init__.py
+++ distnet/keras_models/__init__.py
@@ -2,3 +2,4 @@ name="keras_models"
 from .unet import get_distnet_model, get_unet_model, get_custom_unet_model
 from .self_attention import SelfAttention
 from .multihead_self_attention import MultiHeadSelfAttention
+from .layers import ConstantConvolution2D
